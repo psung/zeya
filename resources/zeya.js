aa9c9318951b5af93591b851c00fa850ef0c5d2c
Phil Sung <philbert@gmail.com>
2009-07-20 00:52:25 -0700

    Add comments to zeya.js.

--- resources/zeya.js
+++ resources/zeya.js
@@ -1,13 +1,21 @@
-var current_index;
+// Javascript implementation for Zeya client.
+
+// Representation of library.
 var library;
+// Index (into library) of the currently playing song, or null if no song is
+// playing.
+var current_index;
+// Audio object we'll use for playing songs.
 var audio;
 
+// Clear all the children of c.
 function clearChildren(c) {
     while (c.childNodes.length >= 1) {
         c.removeChild(c.firstChild);
     }
 }
 
+// Initialize the application.
 function init() {
     current_index = null;
     library = null;
@@ -16,6 +24,7 @@ function init() {
     load_collection();
 }
 
+// Request the collection from the server and render a table to display it.
 function load_collection() {
     var t = document.getElementById('collection_table');
     var req = new XMLHttpRequest();
@@ -23,6 +32,7 @@ function load_collection() {
     req.onreadystatechange = function(e) {
         if (req.readyState == 4 && req.status == 200) {
             library = JSON.parse(req.responseText);
+            // Each item will have one row in the table.
             for (var index = 0; index < library.length; index++) {
                 var item = library[index];
 
@@ -49,6 +59,7 @@ function load_collection() {
     req.send(null);
 }
 
+// Pause the currently playing song.
 function pause() {
     if (current_index != null) {
         audio.pause();
@@ -56,6 +67,7 @@ function pause() {
     }
 }
 
+// Start or resume playing the current song.
 function play() {
     if (current_index != null) {
         audio.play();
@@ -63,21 +75,24 @@ function play() {
     }
 }
 
+// Load the song with the given index.
 function select_item(index) {
-    current_index = index;
+    // Pause the currently playing song.
     if (audio != null) {
         audio.pause();
         set_ui_state('pause');
     }
+    // Start streaming the new song.
     entry = library[index];
     audio = new Audio('/getcontent?' + escape(entry.key));
     audio.setAttribute('autoplay', 'true');
-    if (current_index < library.length - 1) {
-        audio.addEventListener('ended',
-                               select_item_fn(current_index + 1),
-                               false);
+    current_index = index;
+    // When this song is finished, advance to the next one.
+    if (index < library.length - 1) {
+        audio.addEventListener('ended', select_item_fn(index + 1), false);
     }
     audio.load();
+    // Update the UI.
     clearChildren(document.getElementById('title_text'));
     clearChildren(document.getElementById('artist_text'));
     document.getElementById('title_text').appendChild(document.createTextNode(entry.title));
@@ -86,6 +101,7 @@ function select_item(index) {
     set_ui_state('play');
 }
 
+// Wrapper around select_item.
 function select_item_fn(index) {
     var current_index = index;
     return function() {
@@ -93,14 +109,18 @@ function select_item_fn(index) {
     }
 }
 
+// Set the state of the UI.
 function set_ui_state(new_state) {
     if (new_state == 'grayed') {
+        // Both buttons grayed.
         document.getElementById("play_img").className = 'grayed';
         document.getElementById("pause_img").className = 'grayed';
     } else if (new_state == 'play') {
+        // 'Play' depressed
         document.getElementById("play_img").className = 'activated';
         document.getElementById("pause_img").className = '';
     } else {
+        // 'Pause' depressed
         document.getElementById("pause_img").className = 'activated';
         document.getElementById("play_img").className = '';
     }
