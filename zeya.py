63b5aaa5e677542f9df05e58b35b1538f2ec33f0
Phil Sung <philbert@gmail.com>
2009-07-20 23:50:26 -0700

    Add GPLv3 license and headers in Python code.

--- zeya.py
+++ zeya.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Web music server.
+# Copyright (C) 2009 Phil Sung
+#
+# This file is part of Zeya.
+#
+# Zeya is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Zeya is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Zeya. If not, see <http://www.gnu.org/licenses/>.
+
+
+# Zeya - a web music server.
 
 import BaseHTTPServer
 
