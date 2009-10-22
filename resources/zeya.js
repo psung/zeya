c9a8191b80b8041ea06403f96d327e175a517653
Romain Francoise <romain@orebokech.com>
2009-10-21 22:24:05 -0700

    Implement search in Zeya web client.
    Add a search function which works by filtering the collection while it
    is displayed in the page, looking for a search string in the title,
    artist and album fields of each row.
    
    A new search box is added in the top right corner of the page.
    
    Split library loading and table rendering in two separate functions.
    
    Reimplement next/previous movement functions so that they use the DOM to
    walk the table instead of assuming that index values are consecutive
    (which is not the case if the collection has been filtered).
    
    New key binding: / to focus the search box.
    
    Tested in Iceweasel 3.5.3 and Google Chrome 3.0.195.27.

--- resources/zeya.js
+++ resources/zeya.js
@@ -9,6 +9,8 @@ var current_index = null;
 var audio;
 // Current application state ('grayed', 'play', 'pause')
 var current_state = 'grayed';
+// Value of the search box, or null if no search has been performed
+var search_string = null;
 
 // We need to buffer streams for Chrome.
 var using_webkit = navigator.userAgent.indexOf("AppleWebKit") > -1;
@@ -26,14 +28,43 @@ function getRowIdFromIndex(index) {
   return 'row' + index;
 }
 
+// Return the library index corresponding to a given row id.
+function getIndexFromRowId(id) {
+  return id.substring(3);
+}
+
 // Return the class to use for the row corresponding to the given index. This
 // determines the color of the row.
 function getRowClassFromIndex(index) {
   return index % 2 == 0 ? 'evenrow' : 'oddrow';
 }
 
-// Request the collection from the server and render a table to display it.
-function load_collection() {
+function item_match(item, match_string) {
+  var s = match_string.toLowerCase();
+  if (item.title.toLowerCase().indexOf(s) == -1
+      && item.artist.toLowerCase().indexOf(s) == -1
+      && item.album.toLowerCase().indexOf(s) == -1) {
+      return false;
+  }
+  return true;
+}
+
+// Request the collection from the server then render it.
+function load_collection()
+{
+  var req = new XMLHttpRequest();
+  req.open('GET', '/getlibrary', true);
+  req.onreadystatechange = function(e) {
+    if (req.readyState == 4 && req.status == 200) {
+      library = JSON.parse(req.responseText);
+      render_collection();
+    }
+  };
+  req.send(null);
+}
+
+// Render a table to display it the collection.
+function render_collection() {
   var t = document.createElement('table');
 
   t.id = "collection_table";
@@ -52,41 +83,67 @@ function load_collection() {
   t_head.appendChild(header_td3);
   t.appendChild(t_head);
 
-  var req = new XMLHttpRequest();
-  req.open('GET', '/getlibrary', true);
-  req.onreadystatechange = function(e) {
-    if (req.readyState == 4 && req.status == 200) {
-      library = JSON.parse(req.responseText);
-      // Each item will have one row in the table.
-      for (var index = 0; index < library.length; index++) {
-        var item = library[index];
+  // Each item will have one row in the table.
+  for (var index = 0, current_line = 0; index < library.length; index++) {
+    var item = library[index];
 
-        var link = document.createElement('a');
-        link.setAttribute('href', '#');
-        link.setAttribute('onclick', 'select_item(' + index + '); return false;');
-        link.appendChild(document.createTextNode(item.title));
-
-        var tr = document.createElement('tr');
-        tr.id = getRowIdFromIndex(index);
-        tr.setAttribute('class', getRowClassFromIndex(index));
-        var td1 = document.createElement('td');
-        var td2 = document.createElement('td');
-        var td3 = document.createElement('td');
-        td1.appendChild(link);
-        td2.appendChild(document.createTextNode(item.artist));
-        td3.appendChild(document.createTextNode(item.album));
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        t.appendChild(tr);
+    if (search_string !== null) {
+      if (!item_match(item, search_string)) {
+        continue;
       }
-      document.getElementById('collection').appendChild(t);
-      document.getElementById('collection').style.display = 'block';
-      document.getElementById('loading').style.display = 'none';
     }
-  };
 
-  req.send(null);
+    current_line++;
+    var link = document.createElement('a');
+    link.setAttribute('href', '#');
+    link.setAttribute('onclick', 'select_item(' + index + '); return false;');
+    link.appendChild(document.createTextNode(item.title));
+
+    var tr = document.createElement('tr');
+    tr.id = getRowIdFromIndex(index);
+    tr.setAttribute('class', getRowClassFromIndex(current_line));
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    td1.appendChild(link);
+    td2.appendChild(document.createTextNode(item.artist));
+    td3.appendChild(document.createTextNode(item.album));
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    t.appendChild(tr);
+  }
+  document.getElementById('collection').appendChild(t);
+  document.getElementById('collection').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
+}
+
+// Clear displayed collection.
+function clear_collection() {
+  clearChildren(document.getElementById('collection'));
+  document.getElementById('collection').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+}
+
+// Update current search string and reload collection.
+function search() {
+  var search_box = document.getElementById('search_box');
+  search_string = search_box.value;
+  search_box.blur();
+  // Redisplay collection, filtering on the search string.
+  clear_collection();
+  // The setTimeout trick is to force the browser to display the loading
+  // message before rendering the collection.
+  window.setTimeout("render_collection()", 1);
+  // Return false to prevent an actual form submit.
+  return false;
+}
+
+function focus_search_box() {
+  var search_box = document.getElementById('search_box');
+  search_box.focus();
+  // Select the text (if any)
+  search_box.select();
 }
 
 // Pause the currently playing song.
@@ -131,6 +188,20 @@ function set_title(title, artist) {
   }
 }
 
+// Return the line number corresponding to this row.
+// Note that this is not the same as the index if a search filter has
+// been applied.
+function get_line_number(element) {
+  var collection = document.getElementById('collection_table');
+  var ret = 0;
+  var e = collection.firstChild;
+  while (e != element) {
+    e = e.nextSibling;
+    ret++;
+  }
+  return ret;
+}
+
 // Load the song with the given index.
 function select_item(index) {
   // Pause the currently playing song.
@@ -141,8 +212,11 @@ function select_item(index) {
   // Update the UI.
   set_spinner_visible(true);
   if (current_index !== null) {
-    document.getElementById(getRowIdFromIndex(current_index)).className =
-      getRowClassFromIndex(current_index);
+    current_row = document.getElementById(getRowIdFromIndex(current_index));
+    if (current_row) {
+      current_row.className =
+        getRowClassFromIndex(get_line_number(current_row));
+    }
   }
   document.getElementById(getRowIdFromIndex(index)).className = 'selectedrow';
   // Start streaming the new song.
@@ -155,7 +229,7 @@ function select_item(index) {
   // Hide the spinner when the song has loaded.
   audio.addEventListener(
     'play', function() {set_spinner_visible(false);}, false);
-  if (index == library.length - 1) {
+  if (is_last_track(index)) {
     // When this song is finished, stop playing (if this was the last song
     // in the list).
     audio.addEventListener('ended', stop, false);
@@ -169,21 +243,55 @@ function select_item(index) {
   set_ui_state('play');
 }
 
+// Check if the song with the given index is the last in the list.
+function is_last_track(index) {
+  var collection = document.getElementById('collection_table');
+  var index_row = document.getElementById(getRowIdFromIndex(index));
+  return index_row == collection.lastChild
+}
+
 // Load the next song in the list (with wraparound).
 function select_next() {
-  if (current_index == library.length - 1) {
-    select_item(0);
+  var collection = document.getElementById('collection_table');
+  var current_row = document.getElementById(getRowIdFromIndex(current_index));
+
+  if (!current_row) {
+    // Display changed since we began playing and the displayed
+    // collection is empty.
+    return;
+  }
+
+  // If on the last row, go back to the first.
+  if (current_row == collection.lastChild) {
+    // The table's firstChild is the heading.
+    select_item(getIndexFromRowId(collection.firstChild.nextSibling.id));
   } else {
-    select_item(current_index + 1);
+    var next_row = current_row.nextSibling;
+    if (next_row) {
+      select_item(getIndexFromRowId(next_row.id));
+    }
   }
 }
 
 // Load the previous song in the list (with wraparound).
 function select_previous() {
-  if (current_index === 0) {
-    select_item(library.length - 1);
+  var collection = document.getElementById('collection_table');
+  var current_row = document.getElementById(getRowIdFromIndex(current_index));
+
+  if (!current_row) {
+    // Display changed since we began playing and the displayed
+    // collection is empty.
+    return;
+  }
+
+  // If on the first row, go to the last.
+  if (current_row == collection.firstChild.nextSibling) {
+    select_item(getIndexFromRowId(collection.lastChild.id));
   } else {
-    select_item(current_index - 1);
+    var previous_row = current_row.previousSibling;
+    if (previous_row) {
+      select_item(getIndexFromRowId(previous_row.id));
+    }
   }
 }
 
@@ -256,6 +364,12 @@ function keypress_handler(e) {
   } else {
     return true;
   }
+  // If editing the search box, don't intercept keypresses.
+  // Note, document.activeElement is an HTML5 feature.
+  if (window.document.activeElement
+      == window.document.getElementById('search_box')) {
+    return true;
+  }
   if (String.fromCharCode(keynum) === ' ') {
     if (current_state == 'play') {
       pause();
@@ -268,6 +382,9 @@ function keypress_handler(e) {
     return false;
   } else if (String.fromCharCode(keynum) == 'k') {
     previous();
+    return false;
+  } else if (String.fromCharCode(keynum) == '/') {
+    focus_search_box();
     return false;
   }
   return true;
