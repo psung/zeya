02af340f493321528a3db9ecbac3463007a10a32
Phil Sung <philbert@gmail.com>
2009-11-12 00:28:15 -0800

    decoders.py: check whether decoder is installed and print a warning if not.

--- decoders.py
+++ decoders.py
@@ -20,6 +20,8 @@
 
 # Logic for selecting which decoder to run for a given file.
 
+import os
+
 # TODO: detect available decoders at startup and disable extensions if they
 # can't be played.
 
@@ -30,10 +32,39 @@ decoders = {
     'ogg': ("/usr/bin/oggdec", "-Q", "-o", "-"),
     }
 
+decoder_messages = {
+    'flac': "/usr/bin/flac not found. Please install 'flac' to play .flac files.",
+    'mp3': "/usr/bin/mpg123 not found. Please install 'mpg123' to play .mp3 files.",
+    'ogg': "/usr/bin/oggdec not found. Please install 'vorbis-tools' to play .ogg files.",
+    }
+
+# The set of extensions for which we've warned the user that the decoder is not
+# available. We maintain this set so we don't prompt the user more than once
+# for the same codec.
+prompts_already_issued = set()
+
 def get_extension(filename):
     # Returns the lowercased extension of the filename (e.g. 'ogg').
     # Raises ValueError if the filename is malformed.
     return filename[filename.rfind('.')+1:].lower()
+
+def has_decoder(filename):
+    """
+    Return whether or not the decoder that would be used to read filename is
+    registered and actually present on the system.
+
+    If it isn't, print a warning, but only if no previous warning has been
+    issued for the same decoder.
+    """
+    if not is_decoder_registered(filename):
+        return False
+    extension = get_extension(filename)
+    decoder_path = decoders[extension][0]
+    decoder_installed = os.path.exists(decoder_path)
+    if not decoder_installed and extension not in prompts_already_issued:
+        print "Warning: %s" % (decoder_messages[extension],)
+        prompts_already_issued.add(extension)
+    return decoder_installed
 
 def is_decoder_registered(filename):
     """
