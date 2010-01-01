d2a4a7edff3b331f6e141f2d57a6af6bd2687ec7
Phil Sung <philbert@gmail.com>
2010-01-01 14:56:00 -0800

    backends.py: add a comment.

--- backends.py
+++ backends.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Zeya. If not, see <http://www.gnu.org/licenses/>.
 
+# Common code for library backends.
+
 import fcntl
 import os
 import signal
