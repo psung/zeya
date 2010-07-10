9d7ad5dc266653b16c41bbd1c40e9d7a54784d37
Phil Sung <philbert@gmail.com>
2010-07-10 15:05:18 -0700

    options.py: make --path default to absolute path of current directory.

--- options.py
+++ options.py
@@ -96,7 +96,7 @@ def get_options(remaining_args):
         print "Warning: --path was set but is ignored for --backend=%s" \
             % (backend_type,)
     if backend_type == 'dir' and path is None:
-        path = "."
+        path = os.getcwd()
     if backend_type == 'playlist' and path is None:
         raise BadArgsError("Specify --path for playlist backend")
     return (help_msg, backend_type, bitrate, port, path, basic_auth_file)
